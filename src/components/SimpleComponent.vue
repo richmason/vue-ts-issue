c60f75d24f9f27633f4c1a278c5045b371865c58
Rich Mason <rich@creativevirtual.com>
2025-03-02 13:00:09 +0800

    add code

---
+++ src/components/SimpleComponent.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const model = defineModel<string | number>({ required: true });
+
+const typeofModel = typeof model.value === "number" ? "number" : "text";
+</script>
+
+<template>
+  <input v-model="model" :type="typeofModel" />
+</template>
